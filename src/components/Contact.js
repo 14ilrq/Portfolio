68f8e907cbbf70ff4427cb7122c8420f73c017be
14ilrq <madilrq@gmail.com>
2026-04-28 20:29:27 +0530

    error r

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
 import { FiMail, FiPhone, FiMapPin, FiLinkedin } from 'react-icons/fi';
 
