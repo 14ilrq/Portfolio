4a77ebaf22eda6b46c2e3316ea00196a52e6a808
14ilrq <madilrq@gmail.com>
2026-04-28 20:17:32 +0530

    rremoved error

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
-import Projects from './components/Projects';
 import Certifications from './components/Certifications';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -44,7 +43,6 @@ function App() {
         <About />
         <Skills />
         <Experience />
-        {/* <Projects /> */}
         <Certifications />
         <Contact />
       </main>
